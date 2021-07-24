dbdc9c9ed06de3069820e4f4f2c7da88f518102a
AKHIL <74488740+akhilnerella81@users.noreply.github.com>
2021-07-24 22:18:49 +0530

    upload csv1

--- csv/app2.js
+++ csv/app2.js
@@ -1,16 +1,4 @@
-const fs = require("fs");
-const express = require("express");
-const bodyParser = require("body-parser");
-const mysql = require("mysql");
-const fastcsv = require("fast-csv");
 
-const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.get("/",function(req,res){
-    res.sendFile(__dirname+"/index.html");
-});
-app.post("/",function(req,res){
 let dropArea = document.getElementById("drop-area");
 
 ["dragenter", "dragover", "dragleave", "drop"].forEach((eventName) => {
@@ -38,7 +26,7 @@ function handleDrop(e) {
   let files = dt.files;
 
   handleFiles(files);
-  DownloadFile(files);
+
 
 }
 
@@ -64,33 +52,3 @@ function uploadFile(file) {
       /* Error. Inform the user */
     });
 }
-
-function DownloadFile(fileName) {
-    //Set the File URL.
-    var url = "Files/" + fileName;
-
-    //Create XMLHTTP Request.
-    var req = new XMLHttpRequest();
-    req.open("GET", url, true);
-    req.responseType = "blob";
-    req.onload = function () {
-        //Convert the Byte Data to BLOB object.
-        var blob = new Blob([req.response], { type: "application/octetstream" });
-
-        //Check the Browser type and download the File.
-        var isIE = false || !!document.documentMode;
-        if (isIE) {
-            window.navigator.msSaveBlob(blob, fileName);
-        } else {
-            var url = window.URL || window.webkitURL;
-            link = url.createObjectURL(blob);
-            var a = document.createElement("a");
-            a.setAttribute("download", fileName);
-            a.setAttribute("href", link);
-            document.body.appendChild(a);
-            a.click();
-            document.body.removeChild(a);
-        }
-    };
-    req.send();
-};
