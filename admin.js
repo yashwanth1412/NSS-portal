508f3246dee14e32717017511797374ab892749c
yashwanth1412 <yashvallabhu@gmail.com>
2021-07-25 16:06:10 +0530

    Csv to db

--- admin.js
+++ admin.js
@@ -19,47 +19,45 @@ router.get("/", (req, res) => {
 })
 
 router.post("/", upload.single("fileName"), async(req, res, next) => {
-    try {
-        if (req.file == undefined) {
-          let err = new Error("Please upload a csv file");
-          throw err;
-        }
-        
-        let path = __dirname + "/records/" + req.file.filename;
-        let csvData = [];
+  try {
+    if (req.file == undefined) {
+      let err = new Error("Please upload a csv file");
+      throw err;
+    }
+    
+    let path = __dirname + "/records/" + req.file.filename;
+    let csvData = [];
 
-        var parser = fs.createReadStream(path)
-        .pipe(csv.parse({ headers: true }))
-        .on("error", (error) => {
-          throw error;
-        })
-        .on("data", (row) => {
-          parser.pause();
-          csvData.push(row);
-          console.log(row)
-          
-          parser.resume();
-        })
-        .on("end", () => {
-          res.send(csvData)
-        });
-      } 
-      catch (err) {
-        next(err)
-      }
+    var event = await Event.create({
+                        name: req.body.event,
+                        date: new Date(req.body.date),
+                        fk_category: req.body.dropdown
+                      })
+
+    var parser = fs.createReadStream(path)
+    .pipe(csv.parse({ headers: true }))
+    .on("error", (error) => {
+      throw error;
+    })
+    .on("data", async(row) => {
+      parser.pause();
+      console.log(row + '\n')
+      await User_Events.create({
+        UserEmail: row.email,
+        EventId: event.id,
+        hours: row.hours
+      })
+
+      parser.resume();
+    })
+    .on("end", () => {
+      res.send(csvData)
+    });
+  } 
+  catch (err) {
+    next(err)
+  }
       
 })
 
 module.exports = router
-
-// Event.create({
-//   name: req.params.event,
-//   date: new Date(),
-//   fk_category: 1
-//   })
-
-// await User_Events.create({
-// UserEmail: req.user.email,
-// EventId: e.id,
-// hours: 5
-// })
