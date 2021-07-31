7f86d6be932da83a992b4211d316c8ea59aaea27
Rohith <rohith.chinnu2000@gmail.com>
2021-07-31 17:37:08 +0530

    first commit

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const path = require("path");
 
 const app = require('./app.js')
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Server started at port 3000");
 });
